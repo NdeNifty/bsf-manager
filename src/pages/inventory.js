cd9b24370b25bc3f4bad57d51165515a177f8a01
NdeNifty <Ndekong@gmail.com>
2023-10-11 11:37:00 +0100

    Layout and fronend pages made

---
+++ src/pages/inventory.js
@@ -0,0 +1,14 @@
+// pages/index.js
+import React from 'react';
+import Layout from '../../components/Layout';
+import Inventory from '../../components/Inventory';
+
+function inventorypage() {
+  return (
+    <Layout>
+      <Inventory />
+    </Layout>
+  );
+};
+
+
