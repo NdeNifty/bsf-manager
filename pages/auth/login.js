18c9873a68fb050237c73f69382cf967f71f8fad
NdeNifty <Ndekong@gmail.com>
2023-10-12 21:45:17 +0100

    shit not working

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -1,5 +1,7 @@
 // pages/auth/login.js
+'use client';
 import React, { useState } from 'react';
+import 'tailwindcss/tailwind.css'
 
 function Login() {
   const [email, setEmail] = useState('');
@@ -7,7 +9,7 @@ function Login() {
 
   const handleEmailLogin = async (e) => {
     e.preventDefault();
-    
+
     // Send a login request to your backend with email/password
     const userData = { email, password };
 
@@ -39,26 +41,45 @@ function Login() {
   };
 
   return (
-    <div>
-      <h1>Login</h1>
-      <form onSubmit={handleEmailLogin}>
-        <label>Email:</label>
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
+    <div className="flex items-center justify-center min-h-screen bg-gray-900">
+      <div className="bg-gray-100 shadow-2xl rounded-lg p-10 w-96">
+        <h1 className="text-3xl font-semibold mb-6 text-gray-900">Login</h1>
+        <form onSubmit={handleEmailLogin} className="space-y-6">
+          <div>
+            <label className="block text-gray-800 text-sm font-medium mb-2">Email:</label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
 
-        <label>Password:</label>
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
+          <div>
+            <label className="block text-gray-800 text-sm font-medium mb-2">Password:</label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
 
-        <button type="submit">Login with Email</button>
-      </form>
-      <button onClick={handleGoogleLogin}>Login with Google</button>
+          <button
+            type="submit"
+            className="bg-blue-700 hover:bg-blue-800 text-white font-bold py-2 px-4 w-full rounded focus:outline-none focus:shadow-outline"
+          >
+            Login with Email
+          </button>
+        </form>
+
+        <button
+          onClick={handleGoogleLogin}
+          className="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 w-full rounded mt-6 focus:outline-none focus:shadow-outline"
+        >
+          Login with Google
+        </button>
+      </div>
     </div>
   );
 }
--- pages/auth/signup.js
+++ pages/auth/signup.js
@@ -1,5 +1,7 @@
+'use client';
 // pages/auth/signup.js
 import React, { useState } from 'react';
+import 'tailwindcss/tailwind.css'
 
 function Signup() {
   const [email, setEmail] = useState('');
@@ -39,27 +41,47 @@ function Signup() {
   };
 
   return (
-    <div>
-      <h1>Signup</h1>
-      <form onSubmit={handleSignup}>
-        <label>Email:</label>
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
+    <div className="flex items-center justify-center min-h-screen bg-gray-800">
+      <div className="bg-white shadow-lg rounded-lg p-8 w-96">
+        <h1 className="text-3xl font-serif mb-6 text-gray-900">Sign Up</h1>
+        <form onSubmit={handleSignup} className="space-y-4">
+          <div className="mb-6">
+            <label className="block text-white text-sm font-bold mb-2">Email:</label>
+            <input
+              className="shadow appearance-none bg-white border border-gray-300 rounded w-full py-2 px-3 text-gray-800 leading-tight focus:outline-none focus:border-gray-500"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
 
-        <label>Password:</label>
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
+          <div className="mb-6">
+            <label className="block text-white text-sm font-bold mb-2">Password:</label>
+            <input
+              className="shadow appearance-none bg-white border border-gray-300 rounded w-full py-2 px-3 text-gray-800 leading-tight focus:outline-none focus:border-gray-500"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
 
-        <button type="submit">Sign Up</button>
+          <div className="flex items-center justify-between">
+            <button
+              type="submit"
+              className="bg-gray-800 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline transition duration-300 ease-in-out"
+            >
+              Sign Up
+            </button>
+          </div>
+        </form>
 
-        <button onClick={handleGoogleSignup}>Sign Up with Google</button>
-      </form>
+        <button
+          onClick={handleGoogleSignup}
+          className="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mt-6 transition duration-300 ease-in-out"
+        >
+          Sign Up with Google
+        </button>
+      </div>
     </div>
   );
 }
