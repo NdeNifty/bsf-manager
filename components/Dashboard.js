427d85f42b4dd5e146fca81e1506274634799fd9
NdeNifty <Ndekong@gmail.com>
2023-11-27 01:55:26 +0100

    Line chart to be fixed

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import 'tailwindcss/tailwind.css'
+import 'tailwindcss/tailwind.css';
+import SalesChart from './dashboard/saleschart';
 import Kpis from './dashboard/kpis';
+
+
 
 const Dashboard = () => {
   const [inventoryData, setInventoryData] = useState({});
@@ -14,6 +17,7 @@ const Dashboard = () => {
   const [expectedEggs, setExpectedEggs] = useState(0);
   const [expectedLarvae, setExpectedLarvae] = useState(0);
   const [expectedPupae, setExpectedPupae] = useState(0);
+  
 
   useEffect(() => {
     // Fetch inventory data and other relevant data from your backend
@@ -48,40 +52,38 @@ const Dashboard = () => {
 
   return (
     <div>
+       <SalesChart />
+      
       <Kpis />
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-      {/* Inventory Data */}
-      <div className="border p-4">
-        <h2 className="text-xl font-semibold mb-2">Inventory Data</h2>
-        <p>Feedstock Availability: {inventoryData.feedstockAvailability} kg</p>
-        <p>Larvae Count: {larvaeCount}</p>
-        <p>Fly Count: {flyCount}</p>
-        <p>Pupae Count: {pupaeCount}</p>
-        <p>Waste Input: {wasteInput}</p>
-      </div>
+      
 
-      {/* Expectations */}
-      <div className="border p-4">
-        <h2 className="text-xl font-semibold mb-2">Expectations</h2>
-        <p>Expected Eggs: {expectedEggs}</p>
-        <p>Expected Larvae: {expectedLarvae}</p>
-        <p>Expected Pupae: {expectedPupae}</p>
-      </div>
+      
 
-      <div className="border p-4">
-        <h2 className="text-xl font-semibold mb-2">Sales Data</h2>
-        <p>Larvae Sales: {larvaeSales}</p>
-        <p>Pupae Sales: {pupaeSales}</p>
-        <p>Eggs Sales: {eggsSales}</p>
-      </div>
+     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+      
+       <SalesChart />
+        <div className="border p-4">
+          <h2 className="text-xl font-semibold mb-2">Expectations</h2>
+          <p>Expected Eggs: {expectedEggs}</p>
+          <p>Expected Larvae: {expectedLarvae}</p>
+          <p>Expected Pupae: {expectedPupae}</p>
+        </div>
 
-      <div className="border p-4">
-        <h2 className="text-xl font-semibold mb-2">Efficiency</h2>
-        <p>Larvae Sales: {larvaeSales}</p>
-        <p>Pupae Sales: {pupaeSales}</p>
-        <p>Eggs Sales: {eggsSales}</p>
-      </div>
-    </div>
+        <div className="border p-4">
+          <h2 className="text-xl font-semibold mb-2">Sales Data</h2>
+          <p>Larvae Sales: {larvaeSales}</p>
+          <p>Pupae Sales: {pupaeSales}</p>
+          <p>Eggs Sales: {eggsSales}</p>
+        </div>
+
+        <div className="border p-4">
+          <h2 className="text-xl font-semibold mb-2">Efficiency</h2>
+          <p>Larvae Sales: {larvaeSales}</p>
+          <p>Pupae Sales: {pupaeSales}</p>
+          <p>Eggs Sales: {eggsSales}</p>
+        </div>
+      </div> 
+      
     </div>
   );
 };
---
+++ components/dashboard/kpis.jsx
@@ -0,0 +1,54 @@
+'use client';
+import React, { useState, useEffect } from 'react';
+import 'tailwindcss/tailwind.css';
+
+const Kpis = () => {
+    const [inventoryData, setInventoryData] = useState({});
+
+    useEffect(() => {
+      // Fetch inventory data from your backend
+      fetch('http://localhost:3001/api/aggregated') // Replace with your API endpoint
+          .then((response) => response.json())
+          .then((data) => {
+             
+               // Process and set the inventory data
+          setInventoryData({
+            larvaeCount: data.inventory.totalLarvaeLeft,
+            pupaeCount: data.inventory.totalPupaeLeft,
+            feedstockAvailable: data.inventory.totalWasteStock,
+            // ... additional processing as needed
+          });
+          
+          })
+          .catch((error) => {
+            console.error('Error fetching inventory data:', error);
+          });
+  }, []);
+  
+    return (
+        <div className="flex flex-wrap  p-2">
+            <div className="flex flex-col items-center p-2 m-2 w-60 bg-blue-500 text-white rounded-sm shadow-xl">
+                <span className="text-lg font-semibold">Larvae</span>
+                <span className="text-2xl font-bold">{inventoryData.larvaeCount} <span>Kg</span></span>
+            </div>
+            <div className="flex flex-col items-center p-2 m-2 w-60 bg-orange-500 text-white rounded-sm shadow-xl">
+                <span className="text-lg font-semibold">Pupae</span>
+                <span className="text-2xl font-bold">{inventoryData.pupaeCount} <span>Kg</span></span>
+            </div>
+            <div className="flex flex-col items-center p-2 m-2 w-60 bg-green-500 text-white rounded-sm shadow-xl">
+                <span className="text-lg font-semibold">Eggs</span>
+                <span className="text-2xl font-bold">1500 <span>Grams</span></span>
+            </div>
+            <div className="flex flex-col items-center p-2 4 m-2 w-60 bg-red-500 text-white rounded-sm shadow-xl">
+                <span className="text-lg font-semibold">Feed Stock</span>
+                <span className="text-2xl font-bold">{inventoryData.feedstockAvailable} <span>Kg</span></span>
+            </div>
+            <div className="flex flex-col items-center p-2 4 m-2 w-60 bg-blue-500 text-white rounded-sm shadow-xl">
+                <span className="text-lg font-semibold">Current Temperature</span>
+                <span className="text-2xl font-bold">56 <span>Degrees</span></span>
+            </div>
+        </div>
+    );
+}
+
+export default Kpis;
