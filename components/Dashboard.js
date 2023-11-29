52319b78cecb8ab46f08d1c1e059eb726d11d6ca
NdeNifty <Ndekong@gmail.com>
2023-11-29 02:26:13 +0100

    ProductionForcastChart added

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -2,7 +2,7 @@ import React from 'react';
 import 'tailwindcss/tailwind.css';
 import dynamic from 'next/dynamic';
 import Kpis from './dashboard/Kpis';
-import ProductionChart from './dashboard/ProductionChart';
+
 
 // Import SalesChart dynamically with SSR disabled
 const SalesChartWithNoSSR = dynamic(() => import('./dashboard/Saleschart'), {
@@ -14,6 +14,9 @@ const ProductionChartWithNoSSR = dynamic(() => import('./dashboard/ProductionCha
 const EfficientcyChartWithNoSSR = dynamic(() => import('./dashboard/EfficiencyChart'), {
   ssr: false
 });
+const ProductionForcastChartWithNoSSR = dynamic(() => import('./dashboard/ProductionForcastChart'), {
+  ssr: false
+});
 const smallColumn = () => {
   // Placeholder for smallColumn component
   return <div>Small Column Content</div>;
@@ -23,14 +26,15 @@ const Dashboard = () => {
   return (
     <div>
       <Kpis />
-      <div className="grid grid-cols-4 w-full">
-        <div className="col-span-3">
+      <div className="grid grid-cols-12 w-full">
+        <div className="col-span-8"> {/* Adjusted to take 9/12 of the width */}
           <SalesChartWithNoSSR />
           <ProductionChartWithNoSSR />
           <EfficientcyChartWithNoSSR />
         </div>
-        <div className="col-span-1">
-          <smallColumn />
+        <div className="col-span-4"> {/* Adjusted to take 3/12 of the width */}
+          <ProductionForcastChartWithNoSSR />
+          
         </div>
       </div>
     </div>
