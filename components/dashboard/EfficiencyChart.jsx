a8dbe4fa1f9f1e876dd3fef8ae2d28eb75510fd2
NdeNifty <Ndekong@gmail.com>
2023-12-02 23:14:09 +0100

    Efficiency chart

--- components/dashboard/EfficiencyChart.jsx
+++ components/dashboard/EfficiencyChart.jsx
@@ -1,76 +1,143 @@
-"use client"
-import React from "react";
-import {
-   AreaChart,
-   Area,
-   YAxis,
-   XAxis,
-   Tooltip,
-   Legend
-   } from "recharts";
-   class EfficiencyChart extends React.Component {
-      data = [
-         {
-            name: "Jan 2019",
-            "Product A": 3432,
-            "Procuct B": 2342
-         },
-         {
-            name: "Feb 2019",
-            "Product A": 2342,
-            "Procuct B": 7746
-         },
-         {
-            name: "Mar 2019",
-            "Product A": 4565,
-            "Procuct B": 2556
-         },
-         {
-            name: "Apr 2019",
-            "Product A": 6654,
-            "Procuct B": 4465
-         },
-         {
-            name: "May 2019",
-            "Product A": 8765,
-            "Procuct B": 5553
-         }
-      ];
-      render() {
-         return (
-            <AreaChart
-               width={730}
-               height={250}
-               data={this.data}
-               margin={{ top: 10, right: 5, left: 0, bottom: 0 }}>
-            <defs>
-               <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-                  <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
-                  <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
-               </linearGradient>
-               <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
-                  <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
-                  <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
-               </linearGradient>
-            </defs>
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
-            <Legend />
-            <Area
-               type="monotone"
-               dataKey="Product A"
-               stroke="#8884d8"
-               fillOpacity={1}
-               fill="url(#colorUv)"/>
-            <Area
-               type="monotone"
-               dataKey="Procuct B"
-               stroke="#82ca9d"
-               fillOpacity={1}
-               fill="url(#colorPv)"/>
-            </AreaChart>
-      );
-   }
-}
+import React, { useState, useEffect } from 'react';
+import { LineChart, Line, YAxis, XAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import moment from 'moment';
+
+const EfficiencyChart = () => {
+  const [efficiencyData, setEfficiencyData] = useState([]);
+  const [timeframe, setTimeframe] = useState('monthly');
+
+  useEffect(() => {
+    fetch('http://localhost:3001/api/all-data-entries')
+      .then((response) => response.json())
+      .then((data) => {
+        const processedData = processData(data, timeframe);
+        setEfficiencyData(processedData);
+      })
+      .catch((error) => {
+        console.error('Error fetching production data:', error);
+      });
+  }, [timeframe]);
+
+  const handleChange = (event) => {
+    setTimeframe(event.target.value);
+  };
+
+  const processData = (rawData, timeframe) => {
+   const groupedData = rawData.reduce((acc, entry) => {
+     // Handle both 'datavalue' and 'dataValue' due to inconsistency in the field names
+     const valueField = entry.datavalue !== undefined ? entry.datavalue : entry.dataValue;
+     if (!entry.dataItem || valueField === undefined) {
+       // Skip entries without dataItem or a value field
+       console.error(`Invalid entry missing dataItem or value:`, entry);
+       return acc;
+     }
+ 
+     const momentDate = moment(entry.date);
+     let periodKey;
+     switch (timeframe) {
+       case 'weekly':
+         periodKey = momentDate.startOf('isoWeek').format('YYYY-MM-DD');
+         break;
+       case 'monthly':
+         periodKey = momentDate.startOf('month').format('YYYY-MM');
+         break;
+       case 'quarterly':
+         periodKey = `Q${momentDate.quarter()} ${momentDate.year()}`;
+         break;
+       case 'yearly':
+         periodKey = momentDate.startOf('year').format('YYYY');
+         break;
+       default:
+         periodKey = momentDate.format('YYYY-MM-DD');
+     }
+ 
+     if (!acc[periodKey]) {
+       acc[periodKey] = {};
+     }
+ 
+     const item = entry.dataItem;
+     const value = parseFloat(valueField); // Convert valueField to a number
+ 
+     if (!isNaN(value)) {
+       acc[periodKey][item] = (acc[periodKey][item] || 0) + value;
+     } else {
+       // If value is not a number, log an error
+       console.error(`Invalid value for dataItem ${item} in entry:`, entry);
+     }
+ 
+     return acc;
+   }, {});
+ 
+   // Calculate efficiency after grouping and summing your data by period
+   return Object.keys(groupedData).map(period => {
+     const periodData = groupedData[period];
+     const larvae = periodData['larvaeHarvested'] || 0;
+     const pupa = periodData['pupaePlanted'] || 0;
+     const feedConsumed = periodData['wasteConsumed'] || 0;
+
+     // Detailed logging
+    console.log(`Period: ${period}`);
+    console.log(`Larvae: ${larvae}, Pupa: ${pupa}, Feed Consumed: ${feedConsumed}`);
+
+ 
+    let efficiency = 0;
+    if (feedConsumed > 0) {
+      efficiency = (larvae + pupa) / feedConsumed;
+    } else {
+      console.log(`Feed consumed is zero or missing for period: ${period}`);
+    }
+
+    console.log(`Efficiency for ${period}: ${efficiency}`);
+     return {
+       date: period,
+       efficiency, // Include efficiency in your data object
+       ...periodData
+     };
+   }).sort((a, b) => moment(a.date).diff(moment(b.date)));
+ };
+ 
+
+  const formatXAxis = (tickItem) => {
+    switch (timeframe) {
+      case 'weekly':
+        return moment(tickItem).format('MMM DD');
+      case 'monthly':
+        return moment(tickItem).format('MMM YYYY');
+      case 'quarterly':
+        return tickItem;
+      case 'yearly':
+        return moment(tickItem).format('YYYY');
+      default:
+        return tickItem;
+    }
+  };
+
+  return (
+    <div>
+      <div className="mb-4">
+        <label htmlFor="timeframe-select" className="mr-2">Select Timeframe:</label>
+        <select id="timeframe-select" value={timeframe} onChange={handleChange} className="p-2 rounded border">
+          <option value="weekly">Weekly</option>
+          <option value="monthly">Monthly</option>
+          <option value="quarterly">Quarterly</option>
+          <option value="yearly">Yearly</option>
+        </select>
+      </div>
+      <LineChart
+        width={730}
+        height={250}
+        data={efficiencyData}
+        margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="date" tickFormatter={formatXAxis} />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Line type="monotone" dataKey="efficiency" stroke="#82ca9d" activeDot={{ r: 8 }} />
+      </LineChart>
+    </div>
+  );
+};
+
 export default EfficiencyChart;
--- components/dashboard/ProductionChart.jsx
+++ components/dashboard/ProductionChart.jsx
@@ -1,76 +1,156 @@
-"use client"
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import {
-   AreaChart,
-   Area,
-   YAxis,
-   XAxis,
-   Tooltip,
-   Legend
-   } from "recharts";
-   class ProductionChart extends React.Component {
-      data = [
-         {
-            name: "Jan 2019",
-            "Product A": 3432,
-            "Procuct B": 2342
-         },
-         {
-            name: "Feb 2019",
-            "Product A": 2342,
-            "Procuct B": 7746
-         },
-         {
-            name: "Mar 2019",
-            "Product A": 4565,
-            "Procuct B": 2556
-         },
-         {
-            name: "Apr 2019",
-            "Product A": 6654,
-            "Procuct B": 4465
-         },
-         {
-            name: "May 2019",
-            "Product A": 8765,
-            "Procuct B": 5553
-         }
-      ];
-      render() {
-         return (
-            <AreaChart
-               width={730}
-               height={250}
-               data={this.data}
-               margin={{ top: 10, right: 5, left: 0, bottom: 0 }}>
-            <defs>
-               <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-                  <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
-                  <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
-               </linearGradient>
-               <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
-                  <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
-                  <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
-               </linearGradient>
+  AreaChart,
+  Area,
+  YAxis,
+  XAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from 'recharts';
+import moment from 'moment';
+
+const ProductionChart = () => {
+  const [productionData, setProductionData] = useState([]);
+  const [timeframe, setTimeframe] = useState('monthly');
+
+  useEffect(() => {
+    fetch('http://localhost:3001/api/all-data-entries')
+      .then((response) => response.json())
+      .then((data) => {
+        const processedData = processData(data, timeframe);
+        setProductionData(processedData);
+      })
+      .catch((error) => {
+        console.error('Error fetching production data:', error);
+      });
+  }, [timeframe]);
+
+  const handleChange = (event) => {
+    setTimeframe(event.target.value);
+  };
+
+  const processData = (rawData, timeframe) => {
+    // This function groups the data based on the timeframe and sums the values for each dataItem
+    const groupedData = rawData.reduce((acc, entry) => {
+      // Handle both 'datavalue' and 'dataValue' due to inconsistency in the field names
+      const valueField = entry.datavalue !== undefined ? entry.datavalue : entry.dataValue;
+      if (!entry.dataItem || valueField === undefined) {
+        // Skip entries without dataItem or a value field
+        console.error(`Invalid entry missing dataItem or value:`, entry);
+        return acc;
+      }
+  
+      const momentDate = moment(entry.date);
+      let periodKey;
+      switch (timeframe) {
+        case 'weekly':
+          periodKey = momentDate.startOf('isoWeek').format('YYYY-MM-DD');
+          break;
+        case 'monthly':
+          periodKey = momentDate.startOf('month').format('YYYY-MM');
+          break;
+        case 'quarterly':
+          periodKey = `Q${momentDate.quarter()} ${momentDate.year()}`;
+          break;
+        case 'yearly':
+          periodKey = momentDate.startOf('year').format('YYYY');
+          break;
+        default:
+          periodKey = momentDate.format('YYYY-MM-DD');
+      }
+  
+      if (!acc[periodKey]) {
+        acc[periodKey] = {};
+      }
+  
+      const item = entry.dataItem;
+      const value = parseFloat(valueField); // Convert valueField to a number
+  
+      if (!isNaN(value)) {
+        acc[periodKey][item] = (acc[periodKey][item] || 0) + value;
+      } else {
+        // If value is not a number, log an error
+        console.error(`Invalid value for dataItem ${item} in entry:`, entry);
+      }
+  
+      return acc;
+    }, {});
+  
+    // Convert the grouped data into an array format suitable for Recharts
+    const processedData = Object.keys(groupedData).map(period => ({
+      date: period,
+      ...groupedData[period]
+    })).sort((a, b) => moment(a.date).diff(moment(b.date)));
+  
+    return processedData;
+  };
+  
+
+  const formatXAxis = (tickItem) => {
+    // Format the tickItem based on the selected timeframe
+    switch (timeframe) {
+      case 'weekly':
+        return moment(tickItem).format('MMM DD');
+      case 'monthly':
+        return moment(tickItem).format('MMM');
+      case 'quarterly':
+        return tickItem;
+      case 'yearly':
+        return moment(tickItem).format('YYYY');
+      default:
+        return tickItem;
+    }
+  };
+
+  return (
+    <div>
+      <div className="mb-4">
+        <label htmlFor="timeframe-select" className="mr-2">Select Timeframe:</label>
+        <select id="timeframe-select" value={timeframe} onChange={handleChange} className="p-2 rounded border">
+          <option value="weekly">Weekly</option>
+          <option value="monthly">Monthly</option>
+          <option value="quarterly">Quarterly</option>
+          <option value="yearly">Yearly</option>
+        </select>
+      </div>
+      <AreaChart
+        width={730}
+        height={250}
+        data={productionData}
+        margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="date" tickFormatter={formatXAxis} />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        {productionData[0] && Object.keys(productionData[0])
+          .filter(key => key !== 'date')
+          .map((key, idx) => (
+            <Area
+              key={key}
+              type="monotone"
+              dataKey={key}
+              stroke={colors[idx % colors.length]}
+              fillOpacity={0.6}
+              fill={`url(#color${key.replace(/\s+/g, '')})`}
+            />
+        ))}
+        {productionData[0] && Object.keys(productionData[0])
+          .filter(key => key !== 'date')
+          .map((key, idx) => (
+            <defs key={key}>
+              <linearGradient id={`color${key.replace(/\s+/g, '')}`} x1="0" y1="0" x2="0" y2="1">
+                <stop offset="5%" stopColor={colors[idx % colors.length]} stopOpacity={0.8}/>
+                <stop offset="95%" stopColor={colors[idx % colors.length]} stopOpacity={0}/>
+              </linearGradient>
             </defs>
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
-            <Legend />
-            <Area
-               type="monotone"
-               dataKey="Product A"
-               stroke="#8884d8"
-               fillOpacity={1}
-               fill="url(#colorUv)"/>
-            <Area
-               type="monotone"
-               dataKey="Procuct B"
-               stroke="#82ca9d"
-               fillOpacity={1}
-               fill="url(#colorPv)"/>
-            </AreaChart>
-      );
-   }
-}
+        ))}
+      </AreaChart>
+    </div>
+  );
+};
+
+const colors = ['#8884d8', '#82ca9d', '#ffc658', '#ff7300', '#a4de6c', '#d0ed57', '#83a6ed'];
+
 export default ProductionChart;
